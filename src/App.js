f3a732badd5e922eb726646b0182a1f41b971cb8
L1ndseyHerman <lherman21@jcu.edu>
2021-09-02 11:21:28 -0400

    Done CSS styling the UsernameAgeForm.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const App = () => {
   return (
     <div className={styles.blackground}>
       <UsernameAgeForm />
-      <UsernameAgeForm />
     </div> 
   );
 }
--- src/components/UsernameAgeForm.js
+++ src/components/UsernameAgeForm.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
-import WhiteCloudDiv from './UI/WhiteCloudDiv';
 import styles from './UsernameAgeForm.module.css';
+
+import WhiteCloudDiv from './UI/WhiteCloudDiv';
+import Button from './UI/Button';
+
 
 const UsernameAgeForm = () => {
     return (
         <WhiteCloudDiv className={styles.centeredStuff}>
-            <h1>Working</h1>
-            <h1>Working</h1>
-            <h1>Working</h1>
-            <h1>Working</h1>
-            <h1>Working</h1>
+            <label className={styles.labelSpacing}>Username:</label>
+            <input type="text" />
+            <br />
+            <br />
+            <label className={styles.labelSpacing}>Age:</label>
+            <input type="text" />
+            <br />
+            <br />
+            <Button />
         </WhiteCloudDiv>
     );
 };
