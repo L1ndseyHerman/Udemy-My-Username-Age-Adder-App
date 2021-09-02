0095a0fc8f6a55bdcdac8d4290d808cfd2fec13e
L1ndseyHerman <lherman21@jcu.edu>
2021-09-01 23:32:50 -0400

    Starting on the WhiteCloudDivs.

---
+++ src/components/WhiteCloudDiv.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './WhiteCloudDiv.module.css';
+
+const WhiteCloudDiv = () => {
+    return (
+    <div className={styles.whiteCloud}>
+        <p>Working</p>
+        <p>Working</p>
+        <p>Working</p>
+        <p>Working</p>
+        <p>Working</p>
+    </div>
+    );
+};
+
+export default WhiteCloudDiv;
